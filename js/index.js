85587eaec6d560fd8eeefe619476b01600ccc514
UlisesRe <ulisesreales8@gmail.com>
2023-08-12 16:06:40 -0300

    Agregando archivos

---
+++ js/index.js
@@ -0,0 +1,85 @@
+
+
+window.sr = ScrollReveal();
+  sr.reveal('.navbar', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-50px',
+});
+
+window.sr = ScrollReveal();
+  sr.reveal('main', {
+    duration: 1500,
+    origin: 'bottom',
+    distance: '-100px'
+});
+
+
+
+window.sr = ScrollReveal();
+  sr.reveal('.content', {
+    duration: 1500,
+    origin: 'top',
+    distance: '-100px'
+});
+
+window.sr = ScrollReveal();
+  sr.reveal('.sport-table-wager', {
+    duration: 1500,
+    origin: 'top',
+    distance: '-100px'
+});
+
+const nombre = document.querySelector(".nombre");
+function cambiaColor(){
+  nombre.style.backgroundcolor="red";
+}
+window.sr = ScrollReveal();
+  sr.reveal('.Studies', {
+    duration: 2500,
+    origin: 'left',
+    distance: '-50px'
+});
+window.sr = ScrollReveal();
+  sr.reveal('.menu-fixed', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-50px',
+});
+
+window.sr = ScrollReveal();
+  sr.reveal('.imagen', {
+    duration: 3000,
+    origin: 'left',
+    distance: '-80px',
+});
+window.sr = ScrollReveal();
+  sr.reveal('.Experience', {
+    duration: 3000,
+    origin: 'right',
+    distance: '-80px',
+});
+window.sr = ScrollReveal();
+  sr.reveal('.More', {
+    duration: 3000,
+    origin: 'left',
+    distance: '-80px',
+});
+window.sr = ScrollReveal();
+  sr.reveal('.cont', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-80px',
+});
+window.sr = ScrollReveal();
+  sr.reveal('.op2', {
+    duration: 3000,
+    origin: 'right',
+    distance: '-80px',
+});
+window.sr = ScrollReveal();
+  sr.reveal('.body', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-50px',
+});
