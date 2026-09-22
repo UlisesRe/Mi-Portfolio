1b4a379b9bdb2c1a0167ceef6dc34d250e6eba98
Ulises Reales <93929923+UlisesRe@users.noreply.github.com>
2026-09-22 16:32:11 -0300

    Add files via upload

---
+++ MiPortfolio/js/index.js
@@ -0,0 +1,68 @@
+
+
+window.sr = ScrollReveal();
+  sr.reveal('.navbar', {
+    duration: 2000,
+    origin: 'top',
+    distance: '50px',
+});
+
+window.sr = ScrollReveal();
+  sr.reveal('main', {
+    duration: 2000,
+    origin: 'left',
+    distance: '100px'
+});
+
+
+
+window.sr = ScrollReveal();
+  sr.reveal('.Studies', {
+    duration: 2500,
+    origin: 'left',
+    distance: '-50px'
+});
+window.sr = ScrollReveal();
+  sr.reveal('.menu-fixed', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-50px',
+});
+
+window.sr = ScrollReveal();
+  sr.reveal('.imagen', {
+    duration: 3000,
+    origin: 'left',
+    distance: '-80px',
+});
+window.sr = ScrollReveal();
+  sr.reveal('.Experience', {
+    duration: 3000,
+    origin: 'right',
+    distance: '-80px',
+});
+window.sr = ScrollReveal();
+  sr.reveal('.More', {
+    duration: 3000,
+    origin: 'left',
+    distance: '-80px',
+});
+
+window.sr = ScrollReveal();
+  sr.reveal('.op2', {
+    duration: 2000,
+    origin: 'top',
+    distance: '80px',
+});
+window.sr = ScrollReveal();
+  sr.reveal('.dropdown', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-50px',
+});
+window.sr = ScrollReveal();
+  sr.reveal('.dibujo', {
+    duration: 2000,
+    origin: 'right',
+    distance: '100px'
+});
